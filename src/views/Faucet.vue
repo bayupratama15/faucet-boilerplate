6cf21618c2ca4a0a56b3bb154e4b87127c728f3b
bayupratama15 <bolehjugaemail@gmail.com>
2022-10-10 17:08:12 +0700

    updated

--- src/views/Faucet.vue
+++ src/views/Faucet.vue
@@ -1,130 +1,126 @@
 <template>
-  <div class="mx-auto container flex justify-center py-16 px-4">
-    <!-- Loading -->
+  <!-- LOADING -->
+  <!-- Loading -->
+  <div
+    v-if="loading"
+    class="flex justify-center items-center h-screen fixed top-0 left-0 right-0 bottom-0 w-full z-50 overflow-hidden bg-gray-700 opacity-75"
+  >
     <div
-      v-if="loading"
-      class="
-        flex
-        justify-center
-        items-center
-        h-screen
-        fixed
-        top-0
-        left-0
-        right-0
-        bottom-0
-        w-full
-        z-50
-        overflow-hidden
-        bg-gray-700
-        opacity-75
-      "
+      class="spinner-border animate-spin inline-block w-8 h-8 rounded-full"
+      role="status"
     >
-      <div
-        class="spinner-border animate-spin inline-block w-8 h-8 rounded-full"
-        role="status"
-      >
-        <span class="visually-hidden">
-          <svg
-            className="animate-spin -inline-block w-8 h-8 border-4 rounded-full"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        </span>
-      </div>
+      <span class="visually-hidden">
+        <svg
+          className="animate-spin -inline-block w-8 h-8 border-4 rounded-full"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </span>
     </div>
-    <!-- Loading -->
-    <div
-      class="
-        p-4
-        max-w-sm
-        bg-white
-        rounded-lg
-        border border-gray-200
-        shadow-md
-        sm:p-6
-        md:p-8
-        dark:bg-gray-800 dark:border-gray-700
-        w-full
-        mt-40
-      "
-    >
-      <div class="space-y-6">
-        <h5
-          class="text-xl font-medium text-gray-900 dark:text-white text-center"
+  </div>
+  <!-- Loading -->
+  <div
+    class="container mx-auto p-6 bg-blue-900 flex h-screen justify-center items-center"
+  >
+    <!-- main container od cards -->
+    <div class="flex-1 p-4">
+      <div
+        class="card w-full h-full bg-transparent hover:bg-gradient-to-r hover: hover:to-[#b2eff7] transition hover:rounded-lg hover:drop-shadow-3xl hover:ease-in-out"
+      >
+        <div
+          class="block h-full overflow-hidden rounded-lg border bg-gradient-to-r from-[#ffa0ae,5%] to-[#b2eff7] opacity-90 shadow-md transition ease-in-out hover:cursor-pointer hover:from-[#ffa0ae,5%] hover:to-[#b2eff7] hover:opacity-100 hover:transition hover:duration-500 sm:flex-row"
         >
-          Faucet Request
-        </h5>
-        <div>
-          <label
-            for="address"
-            class="
-              block
-              mb-2
-              text-sm
-              font-medium
-              text-gray-900
-              dark:text-gray-300
-            "
-            >Your Address</label
-          >
-          <input
-            type="text"
-            name="address"
-            id="address"
-            class="
-              bg-gray-50
-              border border-gray-300
-              text-gray-900 text-sm
-              rounded-lg
-              focus:ring-blue-500 focus:border-blue-500
-              block
-              w-full
-              p-2.5
-              dark:bg-gray-600
-              dark:border-gray-500
-              dark:placeholder-gray-400
-              dark:text-white
-            "
-            placeholder="0xae43bebd1cb0f377a433846cdfb810d28ca189a5"
-            required=""
-            v-model="address"
-          />
+          <div class="p-4 sm:ml-16 md:ml-32 lg:ml-0">
+            <!-- heading -->
+            <div class="-mx-2 flex flex-wrap text-center sm:mx-auto sm:mb-2">
+              <div class="p-2">
+                <div
+                  class="flex h-full items-center justify-center rounded-lg p-4"
+                >
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="3"
+                    class="mr-4 h-6 w-6 flex-shrink-0 text-indigo-500"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                  </svg>
+                  <span
+                    class="title-font text-lg font-bold tracking-wide text-blue-900"
+                  >
+                    Faucet
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div class="lg:px-4 lg:pb-2">
+              <div class="flex flex-wrap -mx-3 mb-6 ml-1">
+                <span class="text-gray-700 text-sm font-bold"
+                  >Enter your address to get 10 tokens</span
+                >
+              </div>
+              <!-- FORM INPUT WITH BUTTON -->
+              <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                  <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-password"
+                  >
+                    Address
+                  </label>
+                  <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                    id="grid-password"
+                    type="text"
+                    placeholder="0x0000000000000000000000000000000000000000"
+                    v-model="address"
+                  />
+                  <p class="text-gray-600 text-xs italic">
+                    Please enter your address
+                  </p>
+                </div>
+              </div>
+              <!-- BUTTON -->
+              <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                  <button
+                    v-if="btnTransaction"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded animate-bounce"
+                    @click="sendTransaction()"
+                  >
+                    Get Tokens
+                  </button>
+                  <button
+                    v-if="btnMetamask"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded animate-bounce"
+                    @click="connectMetamask"
+                  >
+                    Add to Metamask
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <button
-          class="
-            w-full
-            text-white
-            bg-blue-700
-            hover:bg-blue-800
-            focus:ring-4 focus:outline-none focus:ring-blue-300
-            font-medium
-            rounded-lg
-            text-sm
-            px-5
-            py-2.5
-            text-center
-            dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
-          "
-          @click="sendTransaction"
-        >
-          Give Me 1 BREW
-        </button>
       </div>
     </div>
   </div>
@@ -139,6 +135,8 @@ export default {
     return {
       address: "",
       loading: false,
+      btnTransaction: true,
+      btnMetamask: false,
     };
   },
   async mounted() {
@@ -157,6 +155,62 @@ export default {
       );
     }
     this.getAccount();
+    window.ethereum.on("accountsChanged", (accounts) => {
+      this.getAccount();
+    });
+    // DETECTION OF NETWORK IF NOT ON CRYON TESTNET THEN ADD CHAIN ON METAMASK
+    window.ethereum.on("chainChanged", (chainId) => {
+      console.log(chainId);
+      if (chainId === "0x1113") {
+        Swal.fire({
+          icon: "success",
+          title: "You are on the right network",
+          showConfirmButton: true,
+          timer: 1500,
+        });
+        this.getAccount();
+      }
+      if (chainId !== "0x1113") {
+        Swal.fire({
+          title: "Please add Cryon Testnet",
+          text: "Please add Cryon Testnet to your metamask",
+          icon: "warning",
+          confirmButtonText: "Add Cryon Testnet",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.ethereum.request({
+              method: "wallet_addEthereumChain",
+              params: [
+                {
+                  chainId: "0x1113",
+                  chainName: "Cryon Network Testnet",
+                  nativeCurrency: {
+                    name: "Cryon Network",
+                    symbol: "CRYON",
+                    decimals: 18,
+                  },
+                  rpcUrls: ["https://node.cryonetwork.net/"],
+                  blockExplorerUrls: ["https://explorer.cryonetwork.net/"],
+                },
+              ],
+            });
+          }
+        });
+      }
+    });
+    // GET CHAIN ID
+    const chainId = await window.ethereum.request({
+      method: "eth_chainId",
+    });
+    console.log(chainId);
+    // if chainid is not 0x1113 then add chain on metamask
+    if (chainId !== "0x1113") {
+      this.btnTransaction = false;
+      this.btnMetamask = true;
+    } else {
+      this.btnTransaction = true;
+      this.btnMetamask = false;
+    }
   },
   methods: {
     async getAccount() {
@@ -182,10 +236,10 @@ export default {
       }
       this.loading = true;
       const privKey =
-        "c3d93df55ec90bf5469dee4cbfce1101b211172006f8d5a6f3b41c34491772a2";
-      const addressFrom = "0x44B1774e416f279944E05df47D11A194dC1D07b9";
+        "f69d2f1d6f3dbef55d0c204d3c8280209d82a8d1377aa3cc96544ca0f3de7443";
+      const addressFrom = "0x3fE8b14FadD78d379c0cCBB09d7141eb3BE4EBf5";
       const web3 = new Web3(
-        new Web3.providers.HttpProvider("https://rpc.brewhost.org/")
+        new Web3.providers.HttpProvider("https://node.cryonetwork.net/")
       );
       try {
         const nonce = await web3.eth.getTransactionCount(addressFrom);
@@ -195,7 +249,7 @@ export default {
           gasLimit: web3.utils.toHex(1000000),
           gasPrice: web3.utils.toHex(web3.utils.toWei("1", "gwei")),
           to: this.address,
-          value: web3.utils.toHex(web3.utils.toWei("1", "ether")),
+          value: web3.utils.toHex(web3.utils.toWei("10", "ether")),
         };
         const signedTx = await web3.eth.accounts.signTransaction(tx, privKey);
         const receipt = await web3.eth.sendSignedTransaction(
@@ -205,13 +259,13 @@ export default {
         this.loading = false;
         Swal.fire({
           title: "Success",
-          text: "Transaction sent",
+          text: "Transaction sent ",
           icon: "success",
           confirmButtonText: "OK",
         }).then(() => {
           // redirect to tx page target="_blank"
           window.open(
-            `https://explorer.brewhost.org/tx/${receipt.transactionHash}`,
+            `https://explorer.cryonetwork.net/tx/${receipt.transactionHash}`,
             "_blank"
           );
         });
@@ -219,11 +273,30 @@ export default {
         this.loading = false;
         Swal.fire({
           title: "Error",
-          text: "Transaction failed",
+          text: `Transaction failed,${error}`,
           icon: "error",
           confirmButtonText: "OK",
         });
       }
+    },
+    // ADD CRYON TOKEN TO METAMASK
+    async connectMetamask() {
+      window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x1113",
+            chainName: "Cryon Network Testnet",
+            nativeCurrency: {
+              name: "Cryon Network",
+              symbol: "CRYON",
+              decimals: 18,
+            },
+            rpcUrls: ["https://node.cryonetwork.net/"],
+            blockExplorerUrls: ["https://explorer.cryonetwork.net/"],
+          },
+        ],
+      });
     },
   },
 };
